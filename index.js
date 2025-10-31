2d9dc36513d3dd1f82910c289a4466c6b3854868
JotaPe8 <joaopedrolopessardinha@gmail.com>
2025-10-31 14:37:26 -0300

    finish configuration

--- index.js
+++ index.js
@@ -8,6 +8,42 @@ const conn = require('./db/conn')
 const app = express();
 const PORT = 8081;
 
+
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.use(
+    session({
+        name: 'session',
+        secret: 'nosso_secret',
+        resave: false,
+        saveUninitialized: false,
+        store: new FileStore({
+            logFn: () => { },
+            path: require('path').join(require('os').tmpdir(), 'session'),
+        }),
+        cookie: {
+            secure: false,
+            maxAge: 360000,
+            expires: new Date(Date.now() + 360000),
+            httpOnly: true
+        }
+    })
+)
+
+app.use(flash());
+app.use(express.static('public'))
+app.use((req, res, next) => {
+    if (req.session.userId)
+    {
+        res.locals.session = req.session;
+    }
+    next();
+})
+
 conn.sync()
     .then(() => app.listen(PORT))
     .catch(err => console.error(err.message));
